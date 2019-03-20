4ce46ce17ae3a308d16d237fa5283cb8451f7985
Justin James F.dog <jjflor1988@gmil.com>
2019-03-19 18:14:49 -0600

    Added the firmware version file.

---
+++ fw/fw-ver-str.php
@@ -0,0 +1,9 @@
+<?php
+
+	$ver_upper_val="1";
+	$ver_lower_val="0";
+	$ver_upper_not_val="254";
+	$ver_lower_not_val="255";
+	
+	echo sprintf('{<%s>.(%s):n[%s]n.n|%s!n}~', $ver_upper_val, $ver_lower_val, $ver_upper_not_val, $ver_lower_not_val); ?>
+	
